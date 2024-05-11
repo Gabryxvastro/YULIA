faf49f6acabe85033a10786d7b2a872abd1b0a46
Gabryxvastro <166912649+Gabryxvastro@users.noreply.github.com>
2024-05-11 12:57:03 +0200

    Update bot.py
    I modified the code

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,7 +1,26 @@
-import random
-pasw = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-yulie = int (input("Inserisci lunghezza password 10"))
-password = ""
-for i in range(yulie):
-    password += random.choice(pasw)
-print(password)
+import discord
+from discord.ext import commands
+from hio import gen_pass
+
+
+intents = discord.Intents.default()
+intents.message_content - True
+
+bot = commands.Bot(command_prefix = "$", intents=intents)
+
+@bot.event
+
+async def on_ready():
+    print(f"Hai fatto accesso come {bot.user}")
+
+@bot.command()
+
+async def ciao(ctx):
+    await ctx.send(f"Ciao! Sono un bot {bot.user}!")
+
+@bot.command()
+async def pasw(ctx):
+    await ctx.send(gen_pass(10) )
+
+bot.run("INSERT TOKEN")
+
